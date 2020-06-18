ba009426c5eb9a726adfa44bca205d68451c17c6
iglocska <andras.iklody@gmail.com>
2020-06-19 00:36:43 +0200

    fix: [templates] delete url parsing rules fixed

--- app/templates/genericTemplates/delete.php
+++ app/templates/genericTemplates/delete.php
@@ -1,13 +1,13 @@
 <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title"><?= __('Delete {0}', h(Cake\Utility\Inflector::singularize(Cake\Utility\Inflector::humanize($scope)))) ?></h5>
+            <h5 class="modal-title"><?= __('Delete {0}', h(Cake\Utility\Inflector::singularize(Cake\Utility\Inflector::humanize($this->request->getParam('controller'))))) ?></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
         <div class="modal-body">
-            <p><?= __('Are you sure you want to delete {0} #{1}?', h(Cake\Utility\Inflector::singularize($scope)), h($id)) ?></p>
+            <p><?= __('Are you sure you want to delete {0} #{1}?', h(Cake\Utility\Inflector::singularize($this->request->getParam('controller'))), h($id)) ?></p>
         </div>
         <div class="modal-footer">
             <?= $this->Form->postLink(
