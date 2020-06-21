21932dac1eb4f30e484b76a684108927d10e4f28
iglocska <andras.iklody@gmail.com>
2020-06-21 21:29:52 +0200

    new: [login] page added

---
+++ app/templates/Users/login.php
@@ -0,0 +1,11 @@
+<?php
+    echo $this->Html->image('logo.png', ['alt' => 'CakePHP', 'class="form-signin"']);
+    echo '<div class="form-signin">';
+    echo $this->Form->create(null, ['url' => ['controller' => 'users', 'action' => 'login']]);
+    echo $this->Form->control('username', ['label' => false, 'class' => 'form-control', 'placeholder' => __('Username')]);
+    echo $this->Form->control('password', ['type' => 'password', 'label' => false, 'class' => 'form-control', 'placeholder' => __('Password')]);
+    echo $this->Form->submit(__('Submit'), ['class' => 'btn btn-lg btn-primary btn-block']);
+    echo $this->Form->end();
+    echo '</div>';
+?>
+</div>
