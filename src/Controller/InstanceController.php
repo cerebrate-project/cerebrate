44c60942c0d820ab0da4b08cced38ac2a602a825
iglocska <andras.iklody@gmail.com>
2020-06-22 14:28:17 +0200

    chg: [restructure] the application
    - move the app dir's contents to the source
    - update composer for easier installation

--- src/Controller/InstanceController.php
+++ src/Controller/InstanceController.php
@@ -12,6 +12,6 @@ class InstanceController extends AppController
     public function home()
     {
         $this->set('metaGroup', $this->isAdmin ? 'Administration' : 'Cerebrate');
-        $this->set('md', file_get_contents(ROOT . '/../README.md'));
+        $this->set('md', file_get_contents(ROOT . '/README.md'));
     }
 }
