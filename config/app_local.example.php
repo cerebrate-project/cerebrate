ad72115fd556b145bd6d37236be4d10acfb3d4a6
iglocska <andras.iklody@gmail.com>
2020-12-07 13:54:39 +0100

    chg: [config] disable debug by default

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -15,7 +15,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
 
     /*
      * Security and encryption configuration
