a8951ed69eca99e2a1249782ea26a27e0b43b812
mokaddem <sami.mokaddem@circl.lu>
2021-02-24 11:05:23 +0100

    new: [instance] Added first version of database migration plugin

--- src/Controller/InstanceController.php
+++ src/Controller/InstanceController.php
@@ -6,12 +6,18 @@ use App\Controller\AppController;
 use Cake\Utility\Hash;
 use Cake\Utility\Text;
 use \Cake\Database\Expression\QueryExpression;
+use Cake\Event\EventInterface;
 
 class InstanceController extends AppController
 {
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        $this->set('metaGroup', !empty($this->isAdmin) ? 'Cerebrate' : 'Administration');
+    }
+
     public function home()
     {
-        $this->set('metaGroup', $this->isAdmin ? 'Administration' : 'Cerebrate');
         $this->set('md', file_get_contents(ROOT . '/README.md'));
     }
 
@@ -21,5 +27,78 @@ class InstanceController extends AppController
         $data = json_decode($data, true);
         $data['user'] = $this->ACL->getUser();
         return $this->RestResponse->viewData($data, 'json');
+    }
+
+    public function migrationIndex()
+    {
+        $migrationStatus = $this->Instance->getMigrationStatus();
+
+        $this->loadModel('Phinxlog');
+        $status = $this->Phinxlog->mergeMigrationLogIntoStatus($migrationStatus['status']);
+
+        $this->set('status', $status);
+        $this->set('updateAvailables', $migrationStatus['updateAvailables']);
+    }
+
+    public function migrate($version=null) {
+        if ($this->request->is('post')) {
+            if (is_null($version)) {
+                $migrateResult = $this->Instance->migrate();
+            } else {
+                $migrateResult = $this->Instance->migrate(['target' => $version]);
+            }
+            if ($this->ParamHandler->isRest() || $this->ParamHandler->isAjax()) {
+                if ($migrateResult['success']) {
+                    return $this->RestResponse->saveSuccessResponse('instance', 'migrate', false, false, __('Migration sucessful'));
+                } else {
+                    return $this->RestResponse->saveFailResponse('instance', 'migrate', false, $migrateResult['error']);
+                }
+            } else {
+                if ($migrateResult['success']) {
+                    $this->Flash->success(__('Migration sucessful'));
+                    $this->redirect(['action' => 'migrationIndex']);
+                } else {
+                    $this->Flash->error(__('Migration fail'));
+                    $this->redirect(['action' => 'migrationIndex']);
+                }
+            }
+        }
+        $migrationStatus = $this->Instance->getMigrationStatus();
+        $this->set('title',  __n('Run database update?', 'Run all database updates?', count($migrationStatus['updateAvailables'])));
+        $this->set('question', __('The process might take some time.'));
+        $this->set('actionName', __n('Run update', 'Run all updates', count($migrationStatus['updateAvailables'])));
+        $this->set('path', ['controller' => 'instance', 'action' => 'migrate']);
+        $this->render('/genericTemplates/confirm');
+    }
+
+    public function rollback($version=null) {
+        if ($this->request->is('post')) {
+            if (is_null($version)) {
+                $migrateResult = $this->Instance->rollback();
+            } else {
+                $migrateResult = $this->Instance->rollback(['target' => $version]);
+            }
+            if ($this->ParamHandler->isRest() || $this->ParamHandler->isAjax()) {
+                if ($migrateResult['success']) {
+                    return $this->RestResponse->saveSuccessResponse('instance', 'rollback', false, false, __('Rollback sucessful'));
+                } else {
+                    return $this->RestResponse->saveFailResponse('instance', 'rollback', false, $migrateResult['error']);
+                }
+            } else {
+                if ($migrateResult['success']) {
+                    $this->Flash->success(__('Rollback sucessful'));
+                    $this->redirect(['action' => 'migrationIndex']);
+                } else {
+                    $this->Flash->error(__('Rollback fail'));
+                    $this->redirect(['action' => 'migrationIndex']);
+                }
+            }
+        }
+        $migrationStatus = $this->Instance->getMigrationStatus();
+        $this->set('title',  __('Run database rollback?'));
+        $this->set('question', __('The process might take some time.'));
+        $this->set('actionName', __('Run rollback'));
+        $this->set('path', ['controller' => 'instance', 'action' => 'rollback']);
+        $this->render('/genericTemplates/confirm');
     }
 }
--- src/Model/Table/InstanceTable.php
+++ src/Model/Table/InstanceTable.php
@@ -5,6 +5,7 @@ namespace App\Model\Table;
 use App\Model\Table\AppTable;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use Migrations\Migrations;
 
 class InstanceTable extends AppTable
 {
@@ -16,5 +17,44 @@ class InstanceTable extends AppTable
     public function validationDefault(Validator $validator): Validator
     {
         return $validator;
+    }
+
+    public function getMigrationStatus()
+    {
+        $migrations = new Migrations();
+        $status = $migrations->status();
+        $status = array_reverse($status);
+
+        $updateAvailables = array_filter($status, function ($update) {
+            return $update['status'] != 'up';
+        });
+        return [
+            'status' => $status,
+            'updateAvailables' => $updateAvailables,
+        ];
+    }
+
+    public function migrate($version=null) {
+        $migrations = new Migrations();
+        if (is_null($version)) {
+            $migrationResult = $migrations->migrate();
+        } else {
+            $migrationResult = $migrations->migrate(['target' => $version]);
+        }
+        return [
+            'success' => true
+        ];
+    }
+
+    public function rollback($version=null) {
+        $migrations = new Migrations();
+        if (is_null($version)) {
+            $migrationResult = $migrations->rollback();
+        } else {
+            $migrationResult = $migrations->rollback(['target' => $version]);
+        }
+        return [
+            'success' => true
+        ];
     }
 }
