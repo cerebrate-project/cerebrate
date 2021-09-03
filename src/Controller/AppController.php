99c857d5868a6237408d7edba0da7e697f1738e7
mokaddem <sami.mokaddem@circl.lu>
2021-09-03 09:49:20 +0200

    chg: [aclcomponent] Added ACL entry

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -127,6 +127,10 @@ class AppController extends Controller
         $this->request->getParam('prefix');
         $this->set('darkMode', !empty(Configure::read('Cerebrate')['ui.dark']));
         $this->set('baseurl', Configure::read('App.fullBaseUrl'));
+
+        if ($this->modelClass == 'Tags.Tags') {
+            $this->set('metaGroup', !empty($this->isAdmin) ? 'Administration' : 'Cerebrate');
+        }
     }
 
     private function authApiUser(): void
