b96991cf4ac0817442b670a1bcc442aeef53dcd3
Sami Mokaddem <sami.mokaddem@circl.lu>
2021-10-08 16:51:54 +0200

    chg: [user] virtual field for user settings

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -9,6 +9,20 @@ use Authentication\PasswordHasher\DefaultPasswordHasher;
 class User extends AppModel
 {
     protected $_hidden = ['password', 'confirm_password'];
+
+    protected $_virtual = ['user_settings_by_name'];
+
+    protected function _getUserSettingsByName()
+    {
+        $settingsByName = [];
+        if (!empty($this->user_settings)) {
+            foreach ($this->user_settings as $i => $setting) {
+                $settingsByName[$setting->name] = $setting;
+            }
+        }
+        return $settingsByName;
+    }
+
     protected function _setPassword(string $password) : ?string
     {
         if (strlen($password) > 0) {
