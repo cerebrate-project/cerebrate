c647ae95ebd8a5881fb4173f4055f450d1295013
Andras Iklody <andras.iklody@gmail.com>
2021-11-24 13:44:12 +0100

    fix: typo in mysql.sql

--- INSTALL/mysql.sql
+++ INSTALL/mysql.sql
@@ -408,7 +408,7 @@ CREATE TABLE IF NOT EXISTS `audit_logs` (
     KEY `ip` (`ip`),
     KEY `model` (`model`),
     KEY `action` (`action`),
-    KEY `model_id` (`model_id`)
+    KEY `model_id` (`model_id`),
     KEY `created` (`created`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
