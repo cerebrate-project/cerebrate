ffac2ef78bedaa29c23d3eeeb6f99c4753257d45
Luciano Righetti <luciano.righetti@gmail.com>
2021-12-17 17:14:40 +0100

    fix: add missing copyright notice

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
 
+# The MIT License (MIT)
+# Copyright (c) 2016 Giles Hall
+# Source: https://github.com/vishnubob/wait-for-it
+
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
