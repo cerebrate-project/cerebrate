2da9d8f7d275b447c597dcdff306044f516e40fd
iglocska <andras.iklody@gmail.com>
2022-02-18 11:47:33 +0100

    new: [keycloak] log enrollment outcome in the audit log

--- src/Model/Behavior/AuthKeycloakBehavior.php
+++ src/Model/Behavior/AuthKeycloakBehavior.php
@@ -137,6 +137,30 @@ class AuthKeycloakBehavior extends Behavior
                 ]
             ]
         );
+        $logChange = [
+            'username' => $data['username'],
+            'individual_id' => $data['individual_id'],
+            'role_id' => $data['role_id']
+        ];
+        if (!$response->isOk()) {
+            $logChange['error_code'] = $response->getStatusCode();
+            $logChange['error_body'] = $response->getStringBody();
+            $this->_table->auditLogs()->insert([
+                'request_action' => 'enrollUser',
+                'model' => 'User',
+                'model_id' => 0,
+                'model_title' => __('Failed Keycloak enrollment for user {0}', $data['username']),
+                'changed' => $logChange
+            ]);
+        } else {
+            $this->_table->auditLogs()->insert([
+                'request_action' => 'enrollUser',
+                'model' => 'User',
+                'model_id' => 0,
+                'model_title' => __('Successful Keycloak enrollment for user {0}', $data['username']),
+                'changed' => $logChange
+            ]);
+        }
         return true;
     }
 
