6bd9d7d2f0cf78918c27a5afca008c21fedf037b
iglocska <andras.iklody@gmail.com>
2022-08-17 13:46:59 +0200

    chg: [error handler] changed to conform with 4.4

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -35,8 +35,8 @@ use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Datasource\ConnectionManager;
-use Cake\Error\ConsoleErrorHandler;
-use Cake\Error\ErrorHandler;
+use Cake\Error\ErrorTrap;
+use Cake\Error\ExceptionTrap;
 use Cake\Filesystem\File;
 use Cake\Http\ServerRequest;
 use Cake\Log\Log;
@@ -132,9 +132,9 @@ ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
  */
 $isCli = PHP_SAPI === 'cli';
 if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+    (new ErrorTrap(Configure::read('Error')))->register();
 } else {
-    (new ErrorHandler(Configure::read('Error')))->register();
+    (new ExceptionTrap(Configure::read('Error')))->register();
 }
 
 /*
