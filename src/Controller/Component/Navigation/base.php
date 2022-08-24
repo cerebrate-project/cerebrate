fac19e0a3ca1e6b9445f1062b2e6216f48b8a453
iglocska <andras.iklody@gmail.com>
2022-08-24 11:43:36 +0200

    fix: [exception] speculative fix to a check causing a 500

--- src/Controller/Component/Navigation/base.php
+++ src/Controller/Component/Navigation/base.php
@@ -12,7 +12,7 @@ class BaseNavigation
     {
         $this->bcf = $bcf;
         $this->request = $request;
-        if (!empty($this->request->getAttribute('identity')->getIdentifier())) {
+        if (!empty($this->request->getAttribute('identity'))) {
             $this->currentUserId = $this->request->getAttribute('identity')->getIdentifier();
         }
         $this->viewVars = $viewVars;
