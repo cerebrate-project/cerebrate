ca65c4b68e7e69f4be77707df1e6b4dd91b216e0
iglocska <andras.iklody@gmail.com>
2022-09-19 01:39:38 +0200

    fix: [alignments] added an index view template
    - Can't see any usefulness in this, but why not
    
    - As reported by SK-CERT

---
+++ templates/Alignments/index.php
@@ -0,0 +1,43 @@
+<?php
+echo $this->element('genericElements/IndexTable/index_table', [
+    'data' => [
+        'data' => $data,
+        'top_bar' => [
+            'children' => [
+
+            ],
+        ],
+        'fields' => [
+            [
+                'name' => '#',
+                'sort' => 'id',
+                'data_path' => 'id',
+            ],
+            [
+                'name' => __('Individual'),
+                'data_path' => 'individual.email',
+                'url' => '/individuals/view/{{0}}',
+                'url_vars' => ['individual.id']
+            ],
+            [
+                'name' => __('Organisation'),
+                'data_path' => 'organisation.name',
+                'url' => '/organisations/view/{{0}}',
+                'url_vars' => ['organisation.id']
+            ],
+            [
+                'name' => __('Type'),
+                'sort' => 'type',
+                'data_path' => 'type'
+            ],
+        ],
+        'title' => __('User index'),
+        'description' => __('The list of enrolled users in this Cerebrate instance. All of the users have or at one point had access to the system.'),
+        'pull' => 'right',
+        'actions' => [
+
+        ]
+    ]
+]);
+echo '</div>';
+?>
