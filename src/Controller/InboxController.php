3d5508055a5a193529ac6f49c7fe5c16a29bc8fa
Sami Mokaddem <sami.mokaddem@circl.lu>
2022-10-25 14:50:39 +0200

    chg: [inbox:index] Allow filtering index by user.id and user.name

--- src/Controller/InboxController.php
+++ src/Controller/InboxController.php
@@ -16,7 +16,7 @@ use Cake\Http\Exception\ForbiddenException;
 
 class InboxController extends AppController
 {
-    public $filterFields = ['scope', 'action', 'title', 'origin', 'message'];
+    public $filterFields = ['scope', 'action', 'title', 'origin', 'message', 'Users.id', 'Users.username'];
     public $quickFilterFields = ['scope', 'action', ['title' => true], ['message' => true]];
     public $containFields = ['Users'];
 
