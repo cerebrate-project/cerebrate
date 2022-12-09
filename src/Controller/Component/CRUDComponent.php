a3fd138b4dc793b3978b57ed53aa768f00e6c320
Sami Mokaddem <sami.mokaddem@circl.lu>
2022-12-09 11:56:03 +0100

    fix: [crud:edit] Always specify the table alias when fetching by id

--- src/Controller/Component/CRUDComponent.php
+++ src/Controller/Component/CRUDComponent.php
@@ -521,7 +521,7 @@ class CRUDComponent extends Component
                 $params['contain'] = [$params['contain'], 'MetaFields'];
             }
         }
-        $query = $this->Table->find()->where(['id' => $id]);
+        $query = $this->Table->find()->where(["{$this->TableAlias}.id" => $id]);
         if (!empty($params['contain'])) {
             $query->contain($params['contain']);
         }
