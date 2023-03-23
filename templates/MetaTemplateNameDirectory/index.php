7df0169ecf543985abe5451973fa8a1abd6cd4cc
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-03-23 14:44:05 +0100

    fix: [meta-template-name-directory] Do not access property from null object

--- templates/MetaTemplateNameDirectory/index.php
+++ templates/MetaTemplateNameDirectory/index.php
@@ -47,9 +47,13 @@ echo $this->element('genericElements/IndexTable/index_table', [
                 'data_path' => 'meta_template.id',
                 'element' => 'function',
                 'function' => function($row, $viewContext) use ($baseurl) {
-                    return $viewContext->Bootstrap->node('a', [
-                        'href' => h($baseurl . '/metaTemplates/view/' . $row->meta_template->id ?? ''),
-                    ], !empty($row->meta_template->name) ? (sprintf('%s (v%s)', h($row->meta_template->name), h($row->meta_template->version))) :'');
+                    if (!empty($row->meta_template)) {
+                        return $viewContext->Bootstrap->node('a', [
+                            'href' => h($baseurl . '/metaTemplates/view/' . $row->meta_template->id ?? ''),
+                        ], !empty($row->meta_template->name) ? (sprintf('%s (v%s)', h($row->meta_template->name), h($row->meta_template->version))) :'');
+                    } else {
+                        return '';
+                    }
                 }
             ],
         ],
