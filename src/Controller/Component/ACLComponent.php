e3f8c38dcdd5d45963e4ef731880d05386d6acb7
iglocska <andras.iklody@gmail.com>
2023-09-12 10:54:06 +0200

    fix: [org admins] should be able to edit the org

--- src/Controller/Component/ACLComponent.php
+++ src/Controller/Component/ACLComponent.php
@@ -181,7 +181,7 @@ class ACLComponent extends Component
         'Organisations' => [
             'add' => ['perm_admin'],
             'delete' => ['perm_admin'],
-            'edit' => ['perm_admin'],
+            'edit' => ['perm_admin', 'perm_org_admin'],
             'filtering' => ['*'],
             'index' => ['*'],
             'tag' => ['perm_org_admin'],
