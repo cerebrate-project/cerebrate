0baa39fbc8b30b25df8885e92b19efabc0a9d85d
Sami Mokaddem <mokaddem.sami@gmail.com>
2026-01-20 14:51:30 +0100

    new: [instance:homepage] Added message from admin section along with dedicated setting

--- src/View/Helper/MarkdownHelper.php
+++ src/View/Helper/MarkdownHelper.php
@@ -4,10 +4,16 @@ namespace App\View\Helper;
 
 use Cake\View\Helper;
 use Cake\Utility\Hash;
+use Parsedown;
 
 class MarkdownHelper extends Helper
 {
     private $Parsedown = null;
+
+    public function initialize(array $config): void
+    {
+        $this->Parsedown = new Parsedown();
+    }
 
     public function text($input)
     {
